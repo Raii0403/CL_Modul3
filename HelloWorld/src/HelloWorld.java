8e39b8502dc07811c3017af5b8b80a8d1ac9a0a2
Raii0403 <cmardjun@gmail.com>
2025-10-20 04:52:52 +0700

    thrd com

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Modul 3!");
         System.out.println("Git & Java working together!");
-        System.out.println("TesTess");
+        System.out.println("TestesTess");
     }
 }
